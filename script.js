547ed7b580cb2e3235b3b7e8f3c47e7005ba25f4
MarieChanaron <chanaronmarie@gmail.com>
2022-09-16 21:22:17 +0200

    Add an overlay when opening the mobile menu

--- script.js
+++ script.js
@@ -5,11 +5,23 @@ const main = document.querySelector('main');
 const footer = document.querySelector('footer');
 
 function slideIn() {
+    const blackOverlay = document.createElement('div');
+    blackOverlay.style.width = '100vw';
+    blackOverlay.style.height = '100%';
+    blackOverlay.style.backgroundColor = 'RGBa(0, 0, 0, 0.6)';
+    blackOverlay.style.colorBlendMode = 'darken';
+    blackOverlay.style.position = 'absolute';
+    blackOverlay.style.top = 0;
+    blackOverlay.style.left = 0;
+    blackOverlay.classList.add('overlay');
+    main.appendChild(blackOverlay);
     menu.style.right = 0;
     menu.style.transitionDuration = '0.2s';
 }
 
 function slideOut() {
+    const blackOverlay = document.querySelector('div.overlay');
+    main.removeChild(blackOverlay);
     menu.style.right = '-250px';
     menu.style.transitionDuration = '0.2s';
 }
